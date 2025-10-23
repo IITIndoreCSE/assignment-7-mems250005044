b9c9cc28fe28d810b96b45af6a8fe0463b971431
mems250005044 <mems250005044@iiti.ac.in>
2025-10-23 23:38:27 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,37 @@
 /* Write a program that searches for the largest three numbers in an array randomly populated by a user. */
+/* Write a program that searches for the largest three numbers in an array randomly populated by a user. */
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int size;
+  cout<<"Enter the number of elements in the array: ";
+  cin>>size;
+
+  cout<<endl<<"Enter "<<size<<" numbers: "<<endl;
+  int arr[size];
+
+  for(int i = 0; i<size; i++){
+    cout<<endl<<"Element 1: ";
+    cin>>arr[i];
+  }
+
+  for(int i = 0; i<size-1;i++){
+      for(int j = 0; j<size-i-1;j++){
+         if(arr[j]>arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+         }
+      }
+      
+
+   }
+
+   cout<<endl<<"The three largest numbers are:"<<endl;
+   cout<<endl<<"1st: "<<arr[size-1]<<endl;
+   cout<<endl<<"2nd: "<<arr[size-2]<<endl;
+   cout<<endl<<"3rd: "<<arr[size-3]<<endl;
+    return 0;
+}
